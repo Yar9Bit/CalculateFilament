9464c4cafd2046254f363f2a28049adf5ef86692
yar9bit <yar9bit@gmail.com>
2023-03-27 18:29:47 +0500

    Initial commit

---
+++ data.py
@@ -0,0 +1,17 @@
+filament = {
+    'pla': {
+        'density': 1.23
+    },
+    'pet-g': {
+        'density': 1.27
+    },
+    'abs': {
+        'density': 1.08
+    },
+    'hips': {
+        'density': 1.03
+    },
+    'PA12(Nylon)': {
+        'density': 1.14
+    }
+}
---
+++ main.py
@@ -0,0 +1,64 @@
+from tkinter import *
+from tkinter import ttk
+from data import filament
+from math import pi
+
+
+class CombFrame(ttk.Frame):
+    d = 1.75
+
+    def __init__(self, container):
+        super().__init__(container)
+
+        options = {'padx': 10, 'pady': 5}
+        fil_list = [fl for fl in filament]
+
+        self.combobox = ttk.Combobox(values=fil_list)
+        self.combobox.pack(anchor=W, **options)
+
+        self.label = ttk.Label(text='Выбор пластика')
+        self.label.pack(before=self.combobox, anchor=W, **options)
+        self.label_consumption = ttk.Label(font=30)
+        self.label_consumption.pack(anchor=N)
+
+        self.label_ent = ttk.Label(text='Введите длину расхода прутка филамента(мм)')
+        self.ent = ttk.Entry()
+        self.btn_ent = ttk.Button(text='Расчёт расхода пластика', command=self.calculate_consumption)
+        self.ent.pack(**options)
+        self.btn_ent.pack(after=self.ent, **options)
+        self.label_ent.pack(before=self.ent, **options)
+
+        self.pack()
+
+    def calculate_consumption(self) -> None:
+        sel_filament = filament.get(self.combobox.get()).get('density')
+        inp_mm = int(self.ent.get())
+        consumption = ((inp_mm * pi * (self.d ** 2) / 4) * sel_filament)
+        self.label_consumption['text'] = '{0:.3f} грамм'.format(consumption / 1000)
+        return None
+
+
+class MainFrame(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+
+        options = {'padx': 10, 'pady': 10}
+
+        self.label = ttk.Label(self, text='Программа для расчёта расхода пластика', font=22)
+        self.label.pack(**options)
+
+        self.pack(anchor=CENTER, expand=False)
+
+
+class App(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Программа')
+        self.geometry('640x640')
+
+
+if __name__ == '__main__':
+    app = App()
+    frame = MainFrame(app)
+    combobox_frame = CombFrame(app)
+    app.mainloop()
